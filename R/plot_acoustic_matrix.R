bd7e88bc213dcdf27bc747e220e80f2eacb73b6e
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2019-04-23 09:53:14 -0600

    Update plot_acoustic_matrix.R

--- R/plot_acoustic_matrix.R
+++ R/plot_acoustic_matrix.R
@@ -8,6 +8,7 @@
 #' @param transpose  Should the matrix be transposed
 #'
 #' @examples
+#' \dontrun{
 #'   setwd("C:/evans/myanmar/processed_data/PMN")
 #'   filenames <- list.files(getwd(), pattern="csv$", full.names=TRUE)
 #'     filenames <- filenames[grep("PMN", filenames)]
@@ -15,17 +16,43 @@
 #'   pmn <- read.csv(filenames[i])[,-1]
 #'
 #'   plot_acoustic_matrix(pmn, add.time=TRUE, time.int=20)
+#' }
 #'
 #' @export plot_acoustic_matrix
-plot_acoustic_matrix <- function(x, add.time = FALSE, time.int = 20, ncbks = 20,  
+plot_acoustic_matrix <- function(x, add.time = FALSE, time.int = 20, ncbks = 20,
+                                 start.time = NULL, end.time = NULL,  
                                  cols = NULL, transpose = FALSE, ...) {
-	n = nrow(x)
 	if(class(x) != "matrix") x <- as.matrix(x)
-	  if(n > 1440) {
-        warning("Greater than 24 hr sample with one-minute intervals, truncating")
-          x <- x[1:1440,]
+	  n = nrow(x)
+	    dates <- as.POSIXct(strptime("2000-01-01 24:00", "%Y-%m-%d %H:%M"))
+        mins <- seq.POSIXt(dates, (dates + n*60), by = "1 min")[1:n]
+	    hr.min <- as.character(format(mins, "%H:%M"))
+	if(is.null(start.time) & !is.null(end.time)) {
+      message("Start time not defined, defaulting to 24:00")	
+	    start.time = "24:00" 
+	}
+	  if(!is.null(start.time) & is.null(end.time)) {
+	    message(paste0("Sub-setting to defined start time: ", start.time))
+	      sub.idx <- seq(grep(start.time,hr.min)[1], n, by=1)
+	        x <- x[sub.idx,]
+	          dates <- dates[sub.idx] 
+              mins <- mins[sub.idx]
+	          hr.min <- hr.min[sub.idx]
+	  } else if(!is.null(start.time) & !is.null(end.time)) {
+	    message(paste0("Sub-setting to defined start-stop time: ", start.time, " - ", end.time))
+	      sub.idx <- seq(grep(start.time, hr.min)[1], grep(end.time, hr.min)[1], by=1) 
+	        x <- x[sub.idx,]
+	          dates <- dates[sub.idx] 
+              mins <- mins[sub.idx]
+	          hr.min <- hr.min[sub.idx]
+	  } else {
+	    stop("Not a valid time sub-set")
+	  }
+	if(n > 1440) {
+      warning("Greater than 24 hr sample with one-minute intervals, truncating")
+      x <- x[1:1440,]
       }
-	if(n < 1440) 
+	if(n < 1440 & any(is.null(start.time),  is.null(end.time) )) 
       warning("Fewer than 24 hr sample with one-minute intervals")
     if(is.null(cols)) {	  
       cfun <- colorRampPalette(c("black","red","green","blue"), interpolate = "linear")
@@ -33,11 +60,9 @@ plot_acoustic_matrix <- function(x, add.time = FALSE, time.int = 20, ncbks = 20,
     } else {
       cfun <- colorRampPalette(cols, interpolate = "linear")  
     }
-  if(add.time) {
-    dates <- as.POSIXct(strptime("2000-01-01 24:00", "%Y-%m-%d %H:%M"))
-      mins <- seq.POSIXt(dates, (dates + nrow(x)*60), by = "1 min")[1:n]  
-        tidx <- which(as.character(mins) %in% as.character(levels(cut(mins, time.int)))) + time.int
-        axis.labs <- as.character(format(mins, "%H:%M"))[tidx[-length(tidx)]]
+  if(add.time) {  
+    tidx <- which(as.character(mins) %in% as.character(levels(cut(mins, time.int)))) + time.int
+      axis.labs <- as.character(format(mins, "%H:%M"))[tidx[-length(tidx)]]
 	  axis.labs[length(axis.labs)+1] <- as.character(format(mins[n], "%H:%M"))
     axis.idx <- (tidx/max(tidx))
     }  
